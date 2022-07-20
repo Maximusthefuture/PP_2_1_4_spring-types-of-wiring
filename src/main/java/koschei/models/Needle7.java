47030ec3eb2fd6ee3d1709df6a988388869ba71d
MaximusTheFuture <naturemade@yandex.ru>
2022-07-20 21:41:13 +0300

    Добавил в гитигнор

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -13,7 +13,7 @@ public class Needle7 {
     public Needle7(Deth8 deth) {
         this.deth = deth;
     }
-    
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
